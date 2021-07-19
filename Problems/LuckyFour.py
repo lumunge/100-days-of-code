8c3ab08191620ab7ab491b12847b9c65bca8b210
Erick Lumunge <lumungep12@gmail.com>
2021-07-19 07:07:30 +0300

    read me

---
+++ Problems/LuckyFour.py
@@ -0,0 +1,26 @@
+"""
+PROBLEM STATEMENT
+==> Calculate the number of occurrences of digit 4
+    in decimal representation
+"""
+
+def getFour(n):
+    nums = [x for x in str(n)]
+    count = 0
+    for i in nums:
+        if(i == '4'):
+            count += 1
+    return count
+
+#test cases 
+T = int(input())
+for i in range(T):
+    n = int(input())
+    print(getFour(n))
+
+
+
+
+
+
+
