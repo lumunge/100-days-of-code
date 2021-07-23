b6ef30dde182b69b3fe16187fbf00ef85c005576
Erick Lumunge <lumungep12@gmail.com>
2021-07-23 10:48:48 +0300

    Lucky four

--- Problems/LuckyFour.py
+++ Problems/LuckyFour.py
@@ -1,26 +1,26 @@
 """
-PROBLEM STATEMENT
-==> Calculate the number of occurrences of digit 4
-    in decimal representation
+Proof that any four integers a, b, c, d.
+There are two of them whose difference is
+divisible by 3
 """
 
-def getFour(n):
-    nums = [x for x in str(n)]
-    count = 0
-    for i in nums:
-        if(i == '4'):
-            count += 1
-    return count
+import random
+import itertools as it
 
-#test cases 
-T = int(input())
-for i in range(T):
-    n = int(input())
-    print(getFour(n))
+randomNums = []
 
+for i in range(0, 4):
+    n = random.randint(1, 100)
+    randomNums.append(n)
 
+combs = list(it.combinations(randomNums, 2))
 
+print("Random Numbers : ", randomNums)
 
-
+for i in combs:
+    if((i[0] - i[1]) % 3 == 0):
+        print("Sum of ", i[0], " and ", i[1], " : DIVISIBLE by 3")
+    else:
+        print("Sum of ", i[0], " and ", i[1], " : not divisible by 3")
 
 
