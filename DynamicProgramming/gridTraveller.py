d2e5fbd72c8b79c96857d10d12dc16f171397583
Erick Lumunge <lumungep12@gmail.com>
2021-07-24 06:30:10 +0300

    memoized grid traveller

---
+++ DynamicProgramming/gridTraveller.py
@@ -0,0 +1,21 @@
+"""
+BRUTE FORCE SOLN
+def gridTraveller(m, n):
+    if m == 1 and n == 1: return 1
+    if m == 0 or n == 0: return 0
+    return gridTraveller(m-1, n) + gridTraveller(m, n-1)
+"""
+
+#MEMOIZED SOLN
+def gridTraveller(m, n, memo={}):
+    key = m, ',', n
+    if key in memo: return memo[key]
+    if m == 1 and n == 1: return 1
+    if m == 0 or n == 0: return 0
+    memo[key] = gridTraveller(m-1, n, memo) + gridTraveller(m, n-1, memo)
+    return memo[key]
+
+T = int(input())
+for i in range(T):
+    m, n = map(int, input().split())
+    print(gridTraveller(m, n))
