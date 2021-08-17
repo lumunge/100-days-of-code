8d9b84824cac6e17a5d0702b2001bf0b53b1c515
Erick Lumunge <lumungep12@gmail.com>
2021-08-17 20:51:00 +0300

    largest number

--- GreedyAlgorithms/largestNum.cpp
+++ GreedyAlgorithms/largestNum.cpp
@@ -1,40 +1,53 @@
 #include<iostream>
 #include<vector>
+#include<bits/stdc++.h>
 #include<string>
 #include<sstream>
-#include<algorithm>
-#include<limits>
 using namespace std;
 
+bool isGreater(int a, int b){
+    if((to_string(a).append(to_string(b))) > (to_string(b).append(to_string(a))))
+        return true;
+    return false;
+}
+
+int getElement(int key, vector<int> arr){
+    std::vector<int>::iterator it = std::find(arr.begin(), arr.end(), key);
+    if (it != arr.end())
+        return distance(arr.begin(), it);
+    return -1;
+}
+
 int largestNum(int n, vector<int> arr){
-    string result;
+    string answer;
     n = arr.size();
-    while(n > 0){
-        float  maxDigit = -std::numeric_limits<float>::infinity();
+    while(n != 0){
+        int maxDigit = -std::numeric_limits<int>::infinity();
         for(int i = 0; i < n; i++){
-            if(i >= maxDigit){
-                maxDigit = i;
+            if(isGreater(arr[i], maxDigit)){
+                maxDigit = arr[i];
             }
         }
-        result.append(to_string(maxDigit));
-        arr.erase(remove(arr.begin(), arr.end(), maxDigit), arr.end());
+        answer.append(to_string(maxDigit));
+        int elem = getElement(maxDigit, arr);
+        arr.erase(arr.begin() + elem);
         n--;
     }
-    stringstream res(result);
-    int intNum = 0;
-    res >> intNum;
-    return intNum;
+    stringstream res(answer);
+    int result = 0;
+    res >> result;
+    return result;
 }
 
 int main(){
     int n;
+    vector<int> arr;
     int x;
     cin >> n;
-    vector<int> arr;
     for(int i = 0; i < n; i++){
         cin >> x;
         arr.push_back(x);
     }
-    cout << largestNum(n, arr);
+    cout << largestNum(n, arr) << endl;
     return 0;
 }
