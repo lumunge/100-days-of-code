edc569124d31961862ef5cdfe6e6f88d9a91d390
Erick Lumunge <lumungep12@gmail.com>
2021-08-17 20:32:06 +0300

    largest number

--- GreedyAlgorithms/largestNum.cpp
+++ GreedyAlgorithms/largestNum.cpp
@@ -1,11 +1,40 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<limits>
 using namespace std;
 
-bool isGreater(int a, int b){
+int largestNum(int n, vector<int> arr){
+    string result;
+    n = arr.size();
+    while(n > 0){
+        float  maxDigit = -std::numeric_limits<float>::infinity();
+        for(int i = 0; i < n; i++){
+            if(i >= maxDigit){
+                maxDigit = i;
+            }
+        }
+        result.append(to_string(maxDigit));
+        arr.erase(remove(arr.begin(), arr.end(), maxDigit), arr.end());
+        n--;
+    }
+    stringstream res(result);
+    int intNum = 0;
+    res >> intNum;
+    return intNum;
 }
 
-int largestNum(
-
 int main(){
+    int n;
+    int x;
+    cin >> n;
+    vector<int> arr;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        arr.push_back(x);
+    }
+    cout << largestNum(n, arr);
     return 0;
 }
---
+++ GreedyAlgorithms/maxLoot.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n, max;
+    int val, wt;
+    cin >> n >> max;
+    for(int i = 0; i < n; i++)
+        cin >> val >> wt;
+    return 0;
+}
