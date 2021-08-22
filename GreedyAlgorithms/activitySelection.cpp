a7b521894444b2fbe5e82498217ef5df48d0abba
Erick Lumunge <lumungep12@gmail.com>
2021-08-22 23:57:16 +0300

    activity selection

--- GreedyAlgorithms/activitySelection.cpp
+++ GreedyAlgorithms/activitySelection.cpp
@@ -3,9 +3,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+ * PROBLEM STATEMENT
+ * Given a set of activities, along with the starting and finishing time of each activity,
+ * find the maximum number of activities performed by a single person assuming that a 
+ * person can only work on a single activity at a time
+ * 
+ */
+
+//Print array
 void printArr(vector<pair<int, int>> arr){
     for(int i = 0; i < arr.size(); i++){
-        cout << arr[i].first << " -> " << arr[i].second << endl;
+        cout << arr[i].first << " " << arr[i].second << endl;
     }
 }
 
@@ -13,16 +22,21 @@ vector<pair<int, int>> selectActivity(vector<pair<int, int>> arr){
     vector<pair<int, int>> times;
     int j = 0;
     int n = arr.size();
+    //sort array in descending order
     sort(arr.rbegin(), arr.rend());
+    //Push last activity to times array
     times.push_back(make_pair(arr[j].first, arr[j].second));
+    //go through the sorted array comparing elements
+    //for first pair, if first element - 1 == second pair second element
+    //push the pair the pair to times array
     for(int i = 0; i <= n; i++){
         if(arr[j].first - 1 == arr[i].second){
             times.push_back(make_pair(arr[i].first, arr[i].second));
-            j += 1;
+            j += 2;
         }
     }
-
-    printArr(times);
+    //sort in descending order
+    sort(times.begin(), times.end());
     return times;
 }
 
@@ -35,7 +49,7 @@ int main(){
         cin >> start >> end;
         times.push_back(make_pair(start, end));
     }
-    selectActivity(times);
+    printArr(selectActivity(times));
     return 0;
 }
 
