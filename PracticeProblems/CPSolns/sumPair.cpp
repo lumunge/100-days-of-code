8b36026c1169fb95c337c3960afafb7e6139a661
Erick Lumunge <lumungep12@gmail.com>
2021-08-23 15:23:59 +0300

    pair sum

---
+++ PracticeProblems/CPSolns/sumPair.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printArr(vector<pair<int, int>> arr){
+    for(
+
+vector<pair<int, int>> sumPair(vector<int> arr){
+    vector<pair<int, int>> result;
+
+
+int main(){
+    vector<int> arr;
+    int n;
+    int x;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        arr.push_back(x);
+    }
+    return 0;
+}
