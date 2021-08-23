fa9566cbe9f578d0b7e8e91ad11d4a91219384b7
Erick Lumunge <lumungep12@gmail.com>
2021-08-23 03:06:01 +0300

    binary search

--- DivideConquerAlgorithms/binarySearch.cpp
+++ DivideConquerAlgorithms/binarySearch.cpp
@@ -2,21 +2,34 @@
 #include<vector>
 using namespace std;
 
-void printArr(vector<int> arr){
-    for(int i = 0; i < arr.size(); i++){
-        cout << arr[i] << " ";
+//Returns index of target element
+int binarySearch(vector<int> arr, int target){
+    int max = arr.size() - 1;
+    int min = 0;
+    while(min <= max){
+        int guess = (max + min) / 2;
+        if(arr[guess] == target){
+            return guess;
+        }else if(arr[guess] < target){
+            min = guess + 1;
+        }else{
+            max = guess - 1;
+        }
     }
-    cout << endl;
+    return -1;
 }
 
 int main(){
     vector<int> arr;
     int n;
+    int target;
     int x;
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> x;
         arr.push_back(x);
     }
+    cout << "Target: ", cin >> target;
+    cout << binarySearch(arr, target) << endl; 
     return 0;
 }
