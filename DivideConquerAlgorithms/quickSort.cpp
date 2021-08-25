785e09a01f903e27bcc043272a711e85f45d269a
Erick Lumunge <lumungep12@gmail.com>
2021-08-25 19:20:56 +0300

    randomized quicksort

--- DivideConquerAlgorithms/quickSort.cpp
+++ DivideConquerAlgorithms/quickSort.cpp
@@ -1,54 +1,46 @@
-import random
+#include<iostream>
+using namespace std;
 
-def partition3(a, l, r):
-    #write your code here
-    x = a[l]
-    j = l
-    k = r
-    i = l
-    while i <= k:
-        if a[i] < x:
-            a[j], a[i] = a[i], a[j]
-            j += 1
-            i += 1
-        elif a[i] > x:
-            a[i], a[k] = a[k], a[i]
-            k -= 1
-        else:
-            i += 1
-    return j, k
-#    pass
-"""
-def partition2(a, l, r):
-    x = a[l]
-    j = l
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-"""
+//Helper functions
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-def randomized_quick_sort(a, l, r):
-    if l >= r:
-        return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    #use partition3
-    m1, m2 = partition3(a, l, r)
-    randomized_quick_sort(a, l, m1 - 1);
-    randomized_quick_sort(a, m2 + 1, r);
+void printArr(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
+int partition(int arr[], int l, int r){
+    int x = arr[l];
+    int j = l;
+    for(int i = l+1; i <= r; i++){
+        if(arr[i] <= x){
+            j++;
+            swap(&arr[j], &arr[i]);
+        }
+    }
+    swap(&arr[j], &arr[l]);
+    return j;
+}
 
-if __name__ == '__main__':
-    n = int(input())
-    a = list(map(int, input().split()))
-    randomized_quick_sort(a, 0, n - 1)
-    for x in a:
-        print(x, end=' ')
+void quickSort(int arr[], int l, int r){
+    if(l < r){
+        //Partitioning Index
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi-1);
+        quickSort(arr, pi+1, r);
+    }
+}
 
-
-
-
-
+int main(){
+    int arr[] = {8, 9, 5, 4, 1, 3, 10, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+    printArr(arr, n);
+    return 0;
+}
