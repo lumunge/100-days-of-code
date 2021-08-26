42429eef149e6f03a7eb7b59f4f3e5c7fb8aadf0
Erick Lumunge <lumungep12@gmail.com>
2021-08-26 21:14:31 +0300

    binary search

--- DivideConquerAlgorithms/binarySearch.cpp
+++ DivideConquerAlgorithms/binarySearch.cpp
@@ -2,7 +2,11 @@
 #include<vector>
 using namespace std;
 
-//Returns index of target element
+/* PROBLEM STATEMENT
+ * Given a sorted array and keys to search
+ * Return the index of the keys in the array else return -1
+ */
+
 int binarySearch(vector<int> arr, int target){
     int max = arr.size() - 1;
     int min = 0;
@@ -21,15 +25,25 @@ int binarySearch(vector<int> arr, int target){
 
 int main(){
     vector<int> arr;
+    vector<int> searchElements;
+    int m;
     int n;
-    int target;
     int x;
-    cin >> n;
-    for(int i = 0; i < n; i++){
+    int y;
+    cout << "Number of Array elements: ", cin >> m;
+    cout << "Array Element:" << endl;
+    for(int i = 0; i < m; i++){
         cin >> x;
         arr.push_back(x);
     }
-    cout << "Target: ", cin >> target;
-    cout << binarySearch(arr, target) << endl; 
+    cout << "Number of search keys: ", cin >> n;
+    cout << "Search keys: " << endl;
+    for(int i = 0; i < n; i++){
+        cin >> y;
+        searchElements.push_back(y);
+    }
+    for(int i = 0; i < searchElements.size(); i++){
+        cout << binarySearch(arr, searchElements[i]) << endl;
+    }
     return 0;
 }
