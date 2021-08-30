5148c0417fb57fea6fae3ad084f11a15cc8a3c55
Erick Lumunge <lumungep12@gmail.com>
2021-08-30 03:54:32 +0300

    no of inversions

--- DivideConquerAlgorithms/Mergesort.cpp
+++ DivideConquerAlgorithms/Mergesort.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void merge(int arr, int l, int r, int mid){
+void printArr(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void merge(int arr[], int l, int mid, int r){
+    //Create two subarrays arr1 and arr2
     int m1 = mid - l + 1;
     int m2 = r - mid;
 
@@ -11,12 +19,14 @@ void merge(int arr, int l, int r, int mid){
     for(int i = 0; i < m1; i++)
         arr1[i] = arr[l + i];
     for(int i = 0; i < m2; i++)
-        arr2[i] = arr[mid + 1 + j];
-    
+        arr2[i] = arr[mid + 1 + i];
+
+    //Maintain indices of subarrays(i, j) and main array(k)
     int i = 0;
     int j = 0;
     int k = l;
-
+    //Until we reach either end of arr1 or arr2 pick larger element among
+    //elements in arr1 and arr2 and place them in correct position in main array
     while(i < m1 && j < m2){
         if(arr1[i] <= arr2[j]){
             arr[k] = arr1[i];
@@ -27,40 +37,39 @@ void merge(int arr, int l, int r, int mid){
         }
         k++;
     }
-
+    //When we run out of elements in either arr1, or arr2
+    //take the remaining elements and add them to remaining slots in main array
     while(i < m1){
         arr[k] = arr1[i];
         i++;
         k++;
     }
-
     while(j < m2){
         arr[k] = arr2[j];
         j++;
         k++;
     }
-
 }
+
 void mergeSort(int arr[], int l, int r){
     if(l < r){
         int mid = (l+r) / 2;
         mergeSort(arr, l, mid);
         mergeSort(arr, mid+1, r);
-        merge(arr, l, r, mid);
+        merge(arr, l, mid, r);
     }
 }
 
-void printArr(int arr[], int n){
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
 int main(){
-    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int arr[] = {2, 3, 9, 2, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
     printArr(arr, n);
     mergeSort(arr, 0, n-1);
     printArr(arr, n);
     return 0;
 }
+
+
+
+
+
--- DivideConquerAlgorithms/noOfInversions.cpp
+++ DivideConquerAlgorithms/noOfInversions.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 
+int inversions(int arr[], int n){
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i] > arr[j]){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 int main(){
     int arr[] = {2, 3, 9, 2, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int q = partition(arr, 0, n-1);
-    cout << "Q " << q << endl;
+    cout << inversions(arr, n) << endl;
     /*
     quickSort(arr, 0, n-1);
     for(int i = 0; i < n; i++){
