a0711f214a00c5da2c46eda4d4c669542ff3a163
Erick Lumunge <lumungep12@gmail.com>
2021-09-02 22:04:08 +0300

    Move zeros

--- DivideConquerAlgorithms/test.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int main(){
-    vector<int> arr = {2, 3, 5, 6, 8, 9};
-    cout << arr[2];
-    return 0;
-}
--- PracticeProblems/CPSolns/MoveZeros.cpp
+++ PracticeProblems/CPSolns/MoveZeros.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+/*
+ * PROBLEM STATEMENT:
+ * Given an arry move all zeros present in it to end while maintaining a relative order of elements
+ */
 void printArr(int arr[], int n){
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
@@ -8,7 +12,9 @@ void printArr(int arr[], int n){
     cout << endl;
 }
 
+//Soln 1:
 void moveZeros(int arr[], int n){
+//If element is not zero place it in next available position in array
     int j = 0;
     for(int i = 0; i < n; i++){
         if(arr[i] != 0){
@@ -16,16 +22,29 @@ void moveZeros(int arr[], int n){
             j++;
         }
     }
+//Add zeros to remaining part of the array
     for(int i = j; i < n; i++){
         arr[i] = 0;
     }
     printArr(arr, n);
+}
+//Soln2:
+//Select 0 as pivot and partition the array based on pivot
+void partition(int arr[], int n){
+    int j = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] != 0){
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
 }
 
 int main(){
     int arr[] = {5, 8, 0, 9, 0, 1, 0, 2, 0};
     int n = sizeof(arr) / sizeof(arr[n]);
     printArr(arr, n);
-    moveZeros(arr, n);
+    partition(arr, n);
+    printArr(arr, n);
     return 0;
 }
