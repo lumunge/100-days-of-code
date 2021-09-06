ba326d3ce0dab7e8c610fdfa68e1225b05f8493b
Erick Lumunge <lumungep12@gmail.com>
2021-09-06 03:20:47 +0300

    memoization, tabulation

--- DynamicProgramming/CpSolns/fibonacci.cpp
+++ DynamicProgramming/CpSolns/fibonacci.cpp
@@ -2,7 +2,12 @@
 #include<vector>
 using namespace std;
 
+/* NOTES:
+ * Solved by tabulation method:
+ * Soution was derived by computing fib(0), fib(1) .... fib(n) and returning last entry to fibNums table.
+ */
 
+// Fibonacci By Tabulation
 long long fib(int n){
     vector<int> fibNums = {0, 1};
     if(n == 0)
@@ -14,9 +19,36 @@ long long fib(int n){
     return fibNums[n];
 }
 
+// Fibonacci by memoization
+#define NIL -1
+#define MAX 100
+class fibTab{
+public:
+    //Initializing lookup table values as NIL
+    int lookup[MAX];
+
+    void init(){
+        for(int i = 0; i < MAX; i++){
+            lookup[i] = NIL;
+        }
+    }
+
+    long long fib(int n){
+        if(lookup[n] == NIL){
+            if(n <= 1)
+                lookup[n] = n;
+            else
+                lookup[n] = fib(n-1) + fib(n-2);
+        }
+        return lookup[n];
+    }
+};
+
 int main(){
+    fibTab fib;
+    fib.init();
     int n;
     cout << "Enter number: ", cin >> n;
-    cout << fib(n) << endl;
+    cout << fib.fib(n) << endl;
     return 0;
 }
--- DynamicProgramming/README.md
+++ DynamicProgramming/README.md
@@ -4,4 +4,7 @@
      1: Overlapping Subproblems --> Solutions of same subproblems are needed again and again.
      2: Optimal substructure
 
+#### Storing Values for Reuse in DP:
+     1: Memoization(Top Down) --> Make a lookup in table before computing solutions
+     2: Tabulation(Bottom up) --> Builds the solution from first and return the last entry from table.
 
