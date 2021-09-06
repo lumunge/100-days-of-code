c9a3e390c170ecacca9c58c755b1b2585a118fdb
Erick Lumunge <lumungep12@gmail.com>
2021-09-07 01:15:18 +0300

    ugly numbers

--- DivideConquerAlgorithms/Mergesort.cpp
+++ DivideConquerAlgorithms/Mergesort.cpp
@@ -1,73 +1,77 @@
 #include<iostream>
 using namespace std;
 
-void printArr(int arr[], int n){
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
+class MergeSort{
+public:
+    void printArr(int arr[], int n){
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
 
-void merge(int arr[], int l, int mid, int r){
-    //Create two subarrays arr1 and arr2
-    int m1 = mid - l + 1;
-    int m2 = r - mid;
+    void merge(int arr[], int l, int mid, int r){
+        //Create two subarrays arr1 and arr2
+        int m1 = mid - l + 1;
+        int m2 = r - mid;
 
-    int arr1[m1];
-    int arr2[m2];
+        int arr1[m1];
+        int arr2[m2];
 
-    for(int i = 0; i < m1; i++)
-        arr1[i] = arr[l + i];
-    for(int i = 0; i < m2; i++)
-        arr2[i] = arr[mid + 1 + i];
+        for(int i = 0; i < m1; i++)
+            arr1[i] = arr[l + i];
+        for(int i = 0; i < m2; i++)
+            arr2[i] = arr[mid + 1 + i];
 
-    //Maintain indices of subarrays(i, j) and main array(k)
-    int i = 0;
-    int j = 0;
-    int k = l;
-    int count = 0;
-    //Until we reach either end of arr1 or arr2 pick larger element among
-    //elements in arr1 and arr2 and place them in correct position in main array
-    while(i < m1 && j < m2){
-        if(arr1[i] <= arr2[j]){
+        //Maintain indices of subarrays(i, j) and main array(k)
+        int i = 0;
+        int j = 0;
+        int k = l;
+        int count = 0;
+        //Until we reach either end of arr1 or arr2 pick larger element among
+        //elements in arr1 and arr2 and place them in correct position in main array
+        while(i < m1 && j < m2){
+            if(arr1[i] <= arr2[j]){
+                arr[k] = arr1[i];
+                i++;
+            }else{
+                arr[k] = arr2[j];
+                j++;
+                count++;
+            }
+            k++;
+        }
+        //When we run out of elements in either arr1, or arr2
+        //take the remaining elements and add them to remaining slots in main array
+        while(i < m1){
             arr[k] = arr1[i];
             i++;
-        }else{
+            k++;
+        }
+        while(j < m2){
             arr[k] = arr2[j];
             j++;
-            count++;
+            k++;
         }
-        k++;
     }
-    //When we run out of elements in either arr1, or arr2
-    //take the remaining elements and add them to remaining slots in main array
-    while(i < m1){
-        arr[k] = arr1[i];
-        i++;
-        k++;
-    }
-    while(j < m2){
-        arr[k] = arr2[j];
-        j++;
-        k++;
-    }
-}
 
-void mergeSort(int arr[], int l, int r){
-    if(l < r){
-        int mid = (l+r) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid+1, r);
-        merge(arr, l, mid, r);
+    void mergeSort(int arr[], int l, int r){
+        if(l < r){
+            int mid = (l+r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
     }
-}
+};
 
 int main(){
+    MergeSort mergesort;
     int arr[] = {1, 20, 6, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    printArr(arr, n);
-    mergeSort(arr, 0, n-1);
-    printArr(arr, n);
+    mergesort.printArr(arr, n);
+    mergesort.mergeSort(arr, 0, n-1);
+    mergesort.printArr(arr, n);
     return 0;
 }
 
--- DynamicProgramming/CpSolns/uglyNumbers.cpp
+++ DynamicProgramming/CpSolns/uglyNumbers.cpp
@@ -1,40 +1,92 @@
 #include<iostream>
-#include<vector>
+#include<algorithm>
 using namespace std;
 
-class UglyNumber{
+/* PROBLEM STATEMENT
+ * Get nth ugly number:
+ * Numbers whose primes factors are 2, 3, 5
+ */
+
+class UglyNum{
 public:
-    int maxDivide(int a, int b){
+    int divide(int a, int b){
         while(a % b == 0)
-            a /= b;
+            a = a / b;
         return a;
     }
     int isUgly(int n){
-        n = maxDivide(n, 2);
-        n = maxDivide(n, 3);
-        n = maxDivide(n, 5);
+        n = divide(n, 2);
+        n = divide(n, 3);
+        n = divide(n, 5);
         if(n == 1)
             return 1;
         return 0;
     }
     int nthUglyNum(int n){
-        int i = 1;
         int count = 1;
+        int i = 1;
         while(n > count){
             i++;
-            if(isUgly(i)){
+            if(isUgly(i))
                 count++;
+                //cout << "isUgly(" << i << ")" << " count: " << count << endl;
+        }
+        return i;
+    }
+};
+
+/* USING DYNAMIC PROGRAMMING
+ * Declare an array of size n -> sizeo of input
+ * i = 2, j = 3, k = 5
+ * Range for 2 = 2, 4, 6, 8, 10, 12, 14, 16...
+ * Range for 3 = 3, 6, 9, 12, 15, 18, 27...
+ * Range for 5 = 5, 10, 15, 20, 25, 30, 35...
+ * At each iteration select the minimum element of the three
+ * Iteration:
+ *           2 -> 3 -> 4 -> 5 -> 6 -> 8 -> 9 -> 10 -> 12 -> 14 -> 15 -> 16
+ * Since all of the numbers will be multiples of either 2, 3, or 5;
+ */
+
+class UglyNumDP{
+public:
+    unsigned uglyNums(unsigned n){
+        unsigned ugly[n];
+        ugly[0] = 1;
+        unsigned i = 0, j = 0, k = 0;
+        unsigned nextOfTwo = 2;
+        unsigned nextOfThree = 3;
+        unsigned nextOfFive = 5;
+        unsigned nextUglyNum = 1;
+        for(int m = 1; m < n; m++){
+            nextUglyNum = min(nextOfTwo, min(nextOfThree, nextOfFive));
+            ugly[m] = nextUglyNum;
+            if(nextUglyNum == nextOfTwo){
+                i++;
+                nextOfTwo = ugly[i] * 2;
+            }
+            if(nextUglyNum == nextOfThree){
+                j++;
+                nextOfThree = ugly[j] * 3;
+            }
+            if(nextUglyNum == nextOfFive){
+                k++;
+                nextOfFive = ugly[k] * 5;
             }
         }
-        return i;  
+        return nextUglyNum;
     }
+
 };
 
 
 int main(){
-    UglyNumber ugly;
+    UglyNumDP ugly;
     int n;
     cout << "Enter number: ", cin >> n;
-    cout << ugly.nthUglyNum(n) << endl;
+    cout << ugly.uglyNums(n) << endl;
     return 0;
-}
+};
+
+
+
+
