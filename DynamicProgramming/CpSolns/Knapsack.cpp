3edcc8b39cda67f3cec39f5c6f8318a27b250b55
Erick Lumunge <lumungep12@gmail.com>
2021-09-09 03:12:42 +0300

    knapsack without breaking items

--- DynamicProgramming/CpSolns/Knapsack.cpp
+++ DynamicProgramming/CpSolns/Knapsack.cpp
@@ -1,37 +1,61 @@
 #include<iostream>
-#include<bits/stdc++.h>
 #include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-class FractionalKnapsack{
+/* PROBLEM STATEMENT:
+ * Given weights and their values, place these items in knapsack of
+ * specified capacity such that you obtain the maximum value.
+ *
+ * BRUTE FORCE APPROACH:
+ * Consider all subsets of items an calculate total weights,
+ * Then pick the subset with maximum value
+ *
+ * OPTIMAL SUBSTRUCTURE -> Optimal soln to be constructed from optimal solns to subproblems
+ *                      -> Two cases for every item
+ *                      1: Either it is included in final soln.
+ *                      2: Or it is excluded.
+ *
+ * Case 1: Maximum value from n-1 items and capacity(nth item excluded) --> ie: weight > capacity
+ *         Knapsack(values, weights, capacity, n-1)
+ * Case 2: Value of nth item + Maximum value from n-1 items, capacity - weight of nth item.(nth item included)
+ *         values[n-1] + Knapsack(values[n-1], weights, capacity - weights[n-1], n-1)
+ *
+ * Result = Maximum(case1, case2);
+ */
+
+class Knapsack{
 public:
-    int solveKnapsack(const vector<int> &profits, const vector<int> &weights, int capacity){
-        vector<vector<int>> dp(profits.size(), vector<int>(capacity + 1, -1));
-        return this->knapsack(dp, profits, weights, capacity, 0);
+    int maxValue(int values[], int weights[], int capacity, int n){
+        return this->bruteKnapsack(values, weights, capacity, n);
     }
 private:
-    int knapsack(vector<vector<int>> &dp, const vector<int> &profits, const vector<int> &weights, int capacity, int currentIndex){
-        if(capacity <= 0 || currentIndex >= profits.size()){
+    int max(int a, int b){
+        return (a > b) ? a : b;
+    }
+
+    int bruteKnapsack(int values[], int weights[], int capacity, int n){
+        //Base case
+        if(n == 0 || capacity == 0){
             return 0;
         }
-        if(dp[currentIndex][capacity] != -1){
-            return dp[currentIndex][capacity];
-        }
-        int profit1 = 0;
-        if(weights[currentIndex] <= capacity){
-            profit1 = profits[currentIndex] + knapsack(dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1);
-        }
-        int profit2 = knapsack(dp, profits, weights, capacity, currentIndex + 1);
-        dp[currentIndex][capacity] = max(profit1, profit2);
-        return dp[currentIndex][capacity];
+        //If weight is greater than capacity
+        if(weights[n-1] > capacity)
+            return bruteKnapsack(values, weights, capacity, n-1);
+        //Else return max (case1, case2)
+        else
+            return max(bruteKnapsack(values, weights, capacity, n-1), values[n-1] + bruteKnapsack(values, weights, capacity - weights[n-1], n-1)); 
     }
+
 };
 
+
 int main(){
-    FractionalKnapsack knapsack;
-    vector<int> profits = {1, 6, 10, 16};
-    vector<int> weights= {1, 2, 3, 5};
-    int capacity = 7;
-    cout << knapsack.solveKnapsack(profits, weights, capacity) << endl;
+    Knapsack ks;
+    int values []= {160, 100, 120};
+    int weights[] = {10, 20, 30};
+    int n = sizeof(values) / sizeof(values[0]);
+    int capacity = 50;
+    cout << ks.maxValue(values, weights, capacity, n) << endl;
     return 0;
 }
