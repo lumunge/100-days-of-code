9965afe7759ad970ed78f0b0d619c2c88e2e7061
Erick Lumunge <lumungep12@gmail.com>
2021-09-15 12:56:16 +0300

    Extending stack interface solution

---
+++ Datastructures/Stack/stackInterface.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+
+
+int main(){
+    stack<int> stack;
+    int n;
+    cout << "No of operations: ", cin >> n;
+    for(int i = 0; i < n; i++){
+        string cmd;
+        cin >> cmd;
+        if(cmd == "push"){
+            int x;
+            cin >> x;
+            if(!stack.empty() && stack.top() > x){
+                x = stack.top();
+            }
+            stack.push(x);
+        }else if(cmd == "pop"){
+            stack.pop();
+        }else if(cmd == "max"){
+            cout << stack.top() << endl;
+        }
+    }
+    return 0;
+}
+
