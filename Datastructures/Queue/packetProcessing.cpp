7b86d9e187a5ab47bf99a3451445f94dcfdd0e84
Erick Lumunge <lumungep12@gmail.com>
2021-09-17 19:24:58 +0300

    network packet processing

--- Datastructures/Queue/packetProcessing.cpp
+++ Datastructures/Queue/packetProcessing.cpp
@@ -1,88 +1,42 @@
 #include<iostream>
-#include<queue>
 #include<vector>
+#include<queue>
 using namespace std;
 
 
-struct Request{
-    Request(int arrivalTime, int processTime):
-        arrivalTime(arrivalTime),
-        processTime(processTime)
-    {}
-    int arrivalTime;
-    int processTime;
-};
-
-struct Response{
-    Response(bool dropped, int startTime):
-        dropped(dropped),
-        startTime(startTime)
-    {}
-    bool dropped;
-    int startTime;
-};
-
-class Buffer{
-public:
-    Buffer(int size):
-        size(size),
-        finishTime()
-    {}
-
-    Response Process(const Request &request){
-        while(!finishTime.empty() && finishTime.front() <= request.arrivalTime){
-            finishTime.pop_front();
-        }
-        bool dropped = ((this->finishTime.size() == this->size) && (request.arrivalTime < this->finishTime.back()));
-        int startTime = -1;
-        if(!dropped){
-            if(finishTime.empty())
-                startTime = request.arrivalTime;
-            else
-                startTime = max(finishTime.back(), request.arrivalTime);
-            finishTime.push_back(startTime + request.processTime);
-        }
-        return Response(dropped, startTime);
-    }
-private:
-    int size;
-    deque<int> finishTime;
-};
-
-vector<Request> ReadRequests(){
-    vector<Request> requests;
-    int count;
-    cin >> count;
-    for(int i = 0; i < count; ++i){
-        int arrivalTime, processTime;
-        cin >> arrivalTime >> processTime;
-        requests.push_back(Request(arrivalTime, processTime));
-    }
-    return requests;
-}
-
-vector<Response> ProcessRequests(const vector<Request> &requests, Buffer *buffer){
-    vector<Response> responses;
-    for(int i = 0; i < requests.size(); ++i)
-        responses.push_back(buffer->Process(requests[i]));
-    return responses;
-}
-
-void printResponses(const vector<Response> &responses){
-    for(int i = 0; i < responses.size(); ++i)
-        cout << (responses[i].dropped ? -1 : responses[i].startTime) << endl;
-}
 
 int main(){
-    int size;
-    cin >> size;
-    vector<Request> requests = ReadRequests();
-    Buffer buffer(size);
-    vector<Response> responses = ProcessRequests(requests, &buffer);
-    printResponses(responses);
+    int i, j, k, t;
+    int s, n;
+    cin >> s >> n;
+    queue<pair<int, pair<int, int>>> q;
+    vector<pair<int, pair<int, int>>> v(n);
+    vector<int> res(n);
+    for(int i = 0; i < n; i++){
+        cin >> j >> k;
+        v[i] = make_pair(i, make_pair(j, k));
+    }
+    j = 0;
+    while(j < n && j < s){
+        q.push(v[j]);
+        j++;
+    }
+    t = 0;
+    while(!q.empty()){
+        int st = max(t, q.front().second.first);
+        t = st + q.front().second.second;
+        res[q.front().first] = st;
+        q.pop();
+        while(q.size() < s && j < n){
+            if(v[j].second.first < t){
+                res[j++] -= 1;
+            }else{
+                q.push(v[j++]);
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        cout << res[i] << endl;
+    }
     return 0;
 }
-
-
-
-
