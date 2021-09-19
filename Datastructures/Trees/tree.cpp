1ea0869b571e5398e2b5c92fe856decda6176186
Erick Lumunge <lumungep12@gmail.com>
2021-09-19 19:01:54 +0300

    depth first tree traversals

--- Datastructures/Trees/tree.cpp
+++ Datastructures/Trees/tree.cpp
@@ -1,36 +1,63 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 struct Node{
     int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int val){
-        data = val;
+    struct Node *left, *right;
+    Node(int data){
+        this->data = data;
         left = NULL;
         right = NULL;
     }
 };
 
+//Inorder traversal (left, root, right)
+void inorder(struct Node* node){
+    if(node == NULL)
+        return;
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
+}
+
+//Preorder traversal (root, left, right)
+void preorder(struct Node* node){
+    if(node == NULL)
+        return;
+    cout << node->data << " ";
+    preorder(node->left);
+    preorder(node->right);
+}
+//Postorder traversal (left, right, root)
+void postorder(struct Node* node){
+    if(node == NULL)
+        return;
+    postorder(node->left);
+    postorder(node->right);
+    cout << node->data << " ";
+}
+
 int main(){
-    //root creation
     struct Node* root = new Node(1);
-    //create nodes
     root->left = new Node(2);
     root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
     
-    root->left->left - new Node(4);
+    cout << "Inorder traversal" << endl;
+    inorder(root);
+    cout << endl;
+    cout << "Preorder traversal" << endl;
+    preorder(root);
+    cout << endl;
+    cout << "Postorder traversal" << endl;
+    postorder(root);
+    cout << endl;
 
-    /*
-     * nl = NULL
-     *          1
-     *         /  \
-     *        2    3
-     *       / \  /  \
-     *      4  nl nl nl
-     *     / \
-     *    nl nl
-     */
     return 0;
 }
+
+
+
