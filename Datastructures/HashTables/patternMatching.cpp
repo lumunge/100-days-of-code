0840ded8397a6af82823fccd14bc4ef8fd9a3bfa
Erick Lumunge <lumungep12@gmail.com>
2021-10-21 02:27:27 +0300

    rabin karps pattern matching

--- Datastructures/HashTables/patternMatching.cpp
+++ Datastructures/HashTables/patternMatching.cpp
@@ -64,27 +64,52 @@ void hashSearch(string text, string pattern){
 }
 */
 
-
 class PatternSearching{
-    
-    void search(char pattern[], char text[], int bigP){
-        int m = strlen(pattern);
-        int n = strlen(text);
-        int i, j;
-        int p = 0;
-        int t = 0;
-        int h = 1;
+    public:
+        void search(char pattern[], char text[], int bigP){
+            int m = strlen(pattern);
+            int n = strlen(text);
+            int i, j;
+            int p = 0;
+            int t = 0;
+            int h = 1;
 
-        for(i = 0; i < m-1; i++
-    }
+            for(i = 0; i < m-1; i++)
+                h = (h * d) % bigP;
+            
+            //pattern and text hash value
+            for(i = 0; i < m; i++){
+                p = (d * p + pattern[i]) % bigP;
+                t = (d * t + text[i]) % bigP;
+            }
+
+            //Find match in hash value
+            for(i = 0; i <= n-m; i++){
+                if(p == t){
+                    //equal hash values, compare text by text
+                    for(j = 0; j < m; j++){
+                        if(text[i+j] != pattern[j])
+                            break;
+                    }
+                    if(j == m)
+                        cout << i+1 << endl;
+                }
+                //Shift to next window
+                if(i < n - m){
+                    t = (d * (t - text[i] * h) + text[i + m]) % bigP;
+                    //avoid -ves
+                    if(t < 0)
+                        t = (t + bigP);
+                }
+            }
+        }
 };
 
 
-int main(){
-    string text = "AABAACAADAABAABA";
-    string pattern = "AABA";
-    hashSearch(text, pattern);
-    return 0;
+int main() {
+    PatternSearching hashSearch;
+    char text[] = "ABCCDDAEFG";
+    char pattern[] = "CDD";
+    int q = 100000007;
+    hashSearch.search(pattern, text, q);
 }
-
-
