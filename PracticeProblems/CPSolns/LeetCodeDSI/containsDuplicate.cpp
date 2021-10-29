c3f21c131985d21fb9ccf1b1f4fe35005ed1fa00
Erick Lumunge <lumungep12@gmail.com>
2021-10-29 10:16:54 +0300

    contains dupicates, hashmap

--- PracticeProblems/CPSolns/LeetCodeDSI/containsDuplicate.cpp
+++ PracticeProblems/CPSolns/LeetCodeDSI/containsDuplicate.cpp
@@ -10,10 +10,17 @@
  * Input: nums = [3, 6, 8, 0]
  * Output: false
  *
- * Approach:
+ * Approach I:
  * 1. Sort the number array in ascending order.
  * 2. If current element is equal to next element return true, otherwise false.
  *
+ * Approach II:
+ * 1. use an unordered set.
+ * 2. Initilize duplicate as false
+ * 3. Use for loop to go through the nums vector
+ * 4. For each element search it in the unordered set using find()
+ * 5. If its in set, set duplicate to true otherwise insert it to set
+ * 6. Return bool value of duplicate
  */
 
 
@@ -27,12 +34,23 @@ class Solution{
             }
             return false;
         }
+
+        bool containsDuplicateII(std::vector<int>& nums){
+            std::unordered_set<int> uset;
+            bool dup = false;
+            for(int i = 0; i < nums.size(); i++){
+                if(uset.find(nums[i]) != uset.end())
+                    dup = true;
+                else
+                    uset.insert(nums[i]);
+            }
+            return dup;
+        }
 };
 
 int main(){
     Solution soln;
-    std::vector<int> nums = {1, 2, 3, 4};
-    std::cout << soln.containsDuplicate(nums) << std::endl;
+    std::vector<int> nums = {1, 2, 3, 1};
+    std::cout << soln.containsDuplicateII(nums) << std::endl;
     return 0;
 }
-
