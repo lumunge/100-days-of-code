e814525aa691c1eaffb11b0d31456c7ac99dbe8f
Erick Lumunge <lumungep12@gmail.com>
2021-11-02 01:42:49 +0300

    array intersection

--- PracticeProblems/CPSolns/LeetCodeDSI/arrayIntersection.cpp
+++ PracticeProblems/CPSolns/LeetCodeDSI/arrayIntersection.cpp
@@ -1,34 +1,64 @@
 #include<iostream>
 #include<vector>
-#include<set>
+#include<unordered_map>
 #include<algorithm>
 
+using std::unordered_map;
 using std::vector;
-using std::set;
 using std::cout;
 using std::endl;
 
 class Solution{
     public:
-        vector<int> intersect(vector<int>& nums1, vector<int>& nums2){
-            vector<int> result;
-            vector<int> finalRes;
-            for(int i = 0; i < nums1.size(); i++){
-                result.push_back(nums1[i]);
+        vector<int> intersectI(vector<int>& nums1, vector<int>& nums2){
+            sort(nums1.begin(), nums1.end());
+            sort(nums2.begin(), nums2.end());
+
+            int i = 0, j = 0, k = 0;
+
+            while(i < nums1.size() && j < nums2.size()){
+                if(nums1[i] < nums2[j])
+                    i++;
+                else if(nums1[i] > nums2[j])
+                    j++;
+                else{
+                    nums1[k++] = nums1[i];
+                    i++;
+                    j++;
+                }
             }
-            for(int i = 0; i < nums2.size(); i++){
-                if(find(result.begin(), result.end(), nums2[i]) != result.end())
-                    finalRes.push_back(nums2[i]);
-            }
-            return finalRes;
+            return vector<int>(nums1.begin(), nums1.begin() + k);
         }
+
+        vector<int> intersect(vector<int>& nums1, vector<int>& nums2){
+            int m = nums1.size();
+            int n = nums2.size();
+            unordered_map<int, int> umap;
+
+            if(m > n)
+                swap(nums1, nums2);
+            
+            for(auto i:nums1){
+                umap[i]++;
+            }
+
+            int k = 0;
+            for(auto i:nums2){
+                if(umap[i] > 0){
+                    nums1[k++] = i;
+                    --umap[i];
+                }
+            }
+            return vector<int>(nums1.begin(), nums1.begin() + k);
+        }
+            
 };
 
 int main(){
     Solution soln;
     vector<int> nums1 = {4, 9, 5};
     vector<int> nums2 = {9, 4, 9, 8, 4};
-    vector<int> result = soln.intersect(nums1, nums2);
+    vector<int> result = soln.intersectI(nums1, nums2);
     for(int i = 0; i < result.size(); i++){
         cout << result[i] << " ";
     }
---
+++ PracticeProblems/CPSolns/LeetCodeDSI/buySellStock.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+Approach
+
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices){
+        }
+};
+
+int main(){
+    Solution soln;
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << soln.buySell(prices) << endl;
+    return 0;
+}
