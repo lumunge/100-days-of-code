a690a938ced69aeac43ec8e76ba47a550c7b4e81
Erick Lumunge <lumungep12@gmail.com>
2021-11-05 13:50:25 +0300

    searching matrix

--- PracticeProblems/CPSolns/LeetCodeDSI/searchMatrix.cpp
+++ PracticeProblems/CPSolns/LeetCodeDSI/searchMatrix.cpp
@@ -53,6 +53,10 @@ int main(){
     vector<vector<int>> mat = {{1, 3, 5, 7},
                                {10, 11, 16, 20},
                                {23, 30, 34, 60}};
+
+    cout << soln.searchMatrixI(mat, 36) << endl;
+    cout << soln.searchMatrixI(mat, 3) << endl;
+    cout << "using binary search" << endl;
     cout << soln.searchMatrixII(mat, 3) << endl;
     cout << soln.searchMatrixII(mat, 27) << endl;
     return 0;
