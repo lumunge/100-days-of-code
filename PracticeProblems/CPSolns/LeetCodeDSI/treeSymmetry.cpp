508f2eb84730b5be601966a759381688c28ea7c7
Erick Lumunge <lumungep12@gmail.com>
2021-11-09 18:44:25 +0300

    symmetric tree

--- PracticeProblems/CPSolns/LeetCodeDSI/treeSymmetry.cpp
+++ PracticeProblems/CPSolns/LeetCodeDSI/treeSymmetry.cpp
@@ -1,44 +1,46 @@
 #include<iostream>
-#include<vector>
-#include<stack>
 #include "./treeStructure.cpp"
 
-using std::stack;
-using std::vector;
 using std::cout;
 using std::endl;
 
-class Solution {
+/*
+ * PROBLEM STATEMENT:
+ * Given the root of a binary tree, check wheather it is a mirror of itself,
+ * Return true if it is symetrical otherwise false
+ *
+ * Sample:
+ * Input: [1, 2, 2, 3, 4, 4, 3]
+ * Output: true
+ *
+ * Input: 1, 2, 2, null, 3, null, 3]
+ * Output: false
+ *
+ * Approach:
+ * If the tree is empty return true
+ * If either of the subtrees left or right is null, return false
+ * If root nodes data for left and right tree or subtree is not equal return false
+ * Recurse for left and right subtrees
+ */
+
+class Solution{
+    private:
+        bool checker(TreeNode* root1, TreeNode* root2){
+            if(!root1 && !root2)
+                return true;
+            else if(!root1 || !root2)
+                return false;
+            if(root1->data != root2->data)
+                return false;
+            bool lt = checker(root1->right, root2->left);
+            bool rt = checker(root1->left, root2->right);
+            if(!lt || !rt) return false;
+            return true;
+        }
     public:
-        void printVec(vector<int> arr){
-            for(int i = 0; i < arr.size(); i++)
-                cout << arr[i] << " ";
-            cout << endl;
+        bool isSymmetric(TreeNode* root){
+            return checker(root, root);
         }
-
-        vector<int> inOrder(TreeNode* root){
-            if(root == nullptr)
-                return {};
-
-            vector<int> result;
-            stack<TreeNode*> counter;
-            TreeNode* currNode = root;
-            
-            while(!counter.empty() || currNode){
-                if(currNode){
-                    counter.push(currNode);
-                    currNode = currNode->left;
-                }else{
-                    currNode = counter.top();
-                    result.push_back(currNode->data);
-                    counter.pop();
-                    currNode = currNode->right;
-                }
-            }
-            return result;
-        }
-        //bool isSymmetric(TreeNode* root){
-        //}
 
 };
 
@@ -51,8 +53,7 @@ int main(){
     root->left->right = new TreeNode(4);
     root->right->left = new TreeNode(4);
     root->right->right = new TreeNode(3);
-    vector<int> res = soln.inOrder(root);
-    soln.printVec(res);
+    cout << soln.isSymmetric(root) << endl;
     return 0;
 }
 
