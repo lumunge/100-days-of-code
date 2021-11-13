7be772b50681aaa8dacbb62149f7f808f02ba314
Erick Lumunge <lumungep12@gmail.com>
2021-11-14 02:05:43 +0300

    array intersection with hashmap

--- PracticeProblems/JavaSolns/LeetCodeDSI/ArrayIntersection.java
+++ PracticeProblems/JavaSolns/LeetCodeDSI/ArrayIntersection.java
@@ -1,4 +1,7 @@
+//Leetcode 350
+
 import java.util.Arrays;
+import java.util.HashMap;
 
 class ArrayIntersection{
     public static int[] intersection(int[] arr1, int[] arr2){
@@ -19,6 +22,26 @@ class ArrayIntersection{
         }
         return Arrays.copyOfRange(arr1, 0, k);
     }
+    public static int[] intersectionII(int[] arr1, int[] arr2){
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int a = arr1.length, b = arr2.length;
+        for(int i = 0; i < arr1.length; i++){
+            //add to hashmap with count
+            hm.put(arr1[i], hm.getOrDefault(arr1[i], 0) + 1);
+        }
+        int k = 0;
+        for(int i = 0; i < arr2.length; i++){
+            if(hm.getOrDefault(arr2[i], 0) > 0){
+                //push to front of array
+                arr1[k] = arr2[i];
+                k++;
+                //decrement count
+                hm.put(arr2[i], hm.get(arr2[i]) - 1);
+            }
+        }
+        return Arrays.copyOfRange(arr1, 0, k);
+    }
+
     public static void printArr(int arr[]){
         for(int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
@@ -27,7 +50,8 @@ class ArrayIntersection{
     public static void main(String[] args){
         int[] arr1 = {4, 9, 5};
         int[] arr2 = {9, 4, 9, 8, 4};
-        int[] res = intersection(arr1, arr2);
-        printArr(res);
+        //int[] res = intersection(arr1, arr2);
+        int[] res1 = intersectionII(arr1, arr2);
+        printArr(res1);
     }
 }
