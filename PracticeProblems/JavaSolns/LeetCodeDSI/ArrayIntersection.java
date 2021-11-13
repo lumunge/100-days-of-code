61d110c929977976615bf0a7644d99eb26be6575
Erick Lumunge <lumungep12@gmail.com>
2021-11-14 02:17:07 +0300

    array intercestion

--- PracticeProblems/JavaSolns/LeetCodeDSI/ArrayIntersection.java
+++ PracticeProblems/JavaSolns/LeetCodeDSI/ArrayIntersection.java
@@ -25,12 +25,12 @@ class ArrayIntersection{
     public static int[] intersectionII(int[] arr1, int[] arr2){
         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
         int a = arr1.length, b = arr2.length;
-        for(int i = 0; i < arr1.length; i++){
+        for(int i = 0; i < a; i++){
             //add to hashmap with count
             hm.put(arr1[i], hm.getOrDefault(arr1[i], 0) + 1);
         }
         int k = 0;
-        for(int i = 0; i < arr2.length; i++){
+        for(int i = 0; i < b; i++){
             if(hm.getOrDefault(arr2[i], 0) > 0){
                 //push to front of array
                 arr1[k] = arr2[i];
@@ -48,8 +48,8 @@ class ArrayIntersection{
     }
 
     public static void main(String[] args){
-        int[] arr1 = {4, 9, 5};
-        int[] arr2 = {9, 4, 9, 8, 4};
+        int[] arr1 = {4, 7, 9, 7, 6, 7};
+        int[] arr2 = {5, 0, 0, 6, 1, 6, 2, 2, 4};
         //int[] res = intersection(arr1, arr2);
         int[] res1 = intersectionII(arr1, arr2);
         printArr(res1);
