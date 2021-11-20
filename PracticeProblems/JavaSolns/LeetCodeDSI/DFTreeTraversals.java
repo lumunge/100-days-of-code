a0cfae53b885d0bf635fc95fd8ae3d448639bef1
Erick Lumunge <lumungep12@gmail.com>
2021-11-20 19:54:17 +0300

    depth first tree traversals

---
+++ PracticeProblems/JavaSolns/LeetCodeDSI/DFTreeTraversals.java
@@ -0,0 +1,125 @@
+//Depth first tree traversals (preorder, inorder, postorder)
+//Leetcode 144, 94, 145
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
+class TreeNode{
+    int data;
+    TreeNode left, right;
+    public TreeNode(int d){
+        data = d;
+        left = right = null;
+    }
+}
+
+class DFTreeTraversal{
+    static TreeNode root;
+
+    DFTreeTraversal(int data){
+        root = new TreeNode(data);
+    }
+    DFTreeTraversal(){
+        root = null;
+    }
+
+    static List<Integer> preOrder(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> counter = new Stack<>();
+        
+        if(root == null)
+                return result;
+        
+        counter.push(root);
+        TreeNode currNode = root;
+
+        while(!counter.empty()){
+            if(currNode != null){
+                result.add(currNode.data);
+                if(currNode.right != null)
+                    counter.push(currNode.right);
+                currNode = currNode.left;
+            }else{
+                currNode = counter.peek();
+                counter.pop();
+            }
+        }
+        return result;
+    }
+    static List<Integer> inOrder(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> counter = new Stack<>();
+        
+        if(root == null)
+            return result;
+
+        TreeNode currNode = root;
+
+        while(!counter.empty() || currNode != null){
+            if(currNode != null){
+                counter.push(currNode);
+                currNode = currNode.left;
+            }else{
+                currNode = counter.peek();
+                result.add(currNode.data);
+                counter.pop();
+                currNode = currNode.right;
+            }
+        }
+        return result;
+    }
+    static List<Integer> postOrder(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> counter1 = new Stack<>();
+        Stack<Integer> counter2 = new Stack<>();
+        
+        if(root == null)
+                return result;
+
+        counter1.push(root);
+
+        while(!counter1.empty()){
+            TreeNode currNode = counter1.peek();
+            counter1.pop();
+            counter2.push(currNode.data);
+            if(currNode.left != null) counter1.push(currNode.left);
+            if(currNode.right != null) counter1.push(currNode.right);
+        }
+
+        while(!counter2.empty()){
+            result.add(counter2.peek());
+            counter2.pop();
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        DFTreeTraversal tree = new DFTreeTraversal();
+        tree.root = new TreeNode(1);
+        tree.root.right = new TreeNode(2);
+        tree.root.right.left = new TreeNode(3);
+        
+        List<Integer> preOrder = preOrder(root);
+        List<Integer> inOrder = inOrder(root);
+        List<Integer> postOrder = postOrder(root);
+
+        System.out.println("Pre Order df tree traversal");
+        for(Integer i : preOrder)
+            System.out.print(i + " ");
+        System.out.println();
+
+        System.out.println("In Order df tree traversal");
+        for(Integer i : inOrder)
+            System.out.print(i + " ");
+        System.out.println();
+
+        System.out.println("Post Order df tree traversal");
+        for(Integer i : postOrder)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+}
+
+
+
+
