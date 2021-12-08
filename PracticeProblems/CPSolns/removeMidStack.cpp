e972dd7eecd311d67f13f1d26c9143cd817bc01d
Erick Lumunge <lumungep12@gmail.com>
2021-12-08 19:09:39 +0300

    remove middle element in stack

--- PracticeProblems/CPSolns/removeMidStack.cpp
+++ PracticeProblems/CPSolns/removeMidStack.cpp
@@ -7,42 +7,70 @@ using std::stack;
 using std::cout;
 using std::endl;
 
-void removeMid(stack<int> &stack, int n, int curr = 0){
-    //base case (stack is empty, all elements popped)
-    if(stack.empty() || curr == n)
-        return;
+class RemoveMid{
+    public:
+        //iterative approach
+        void removeMidIter(stack<int> &stk){
+            //temporary stack
+            stack<int> temp;
+            int n = stk.size(), curr = 0;
+            //pop from main stack and push to temp stack except where curr = n/2
+            while(!stk.empty()){
+                int i = stk.top();
+                stk.pop();
+                if(curr != n/2){
+                    temp.push(i);
+                }
+                curr += 1;
+            }
+            //pop from temp and push to main stack, no mid element
+            while(!temp.empty()){
+                int t = temp.top();
+                stk.push(t);
+                temp.pop();
+            }
+        }
 
-    //pop current element
-    int i = stack.top();
-    stack.pop();
+        //recursive approach
+        void removeMidRec(stack<int> &stk, int n, int curr = 0){
+            //base case (stack is empty, all elements popped)
+            if(stk.empty() || curr == n)
+                return;
 
-    //recursively remove other elements
-    removeMid(stack, n, curr+1);
+            //pop current element
+            int i = stk.top();
+            stk.pop();
 
-    //add elements back to stack except mid element
-    if(curr != n/2)
-        stack.push(i);
-}
+            //recursively remove other elements
+            removeMidRec(stk, n, curr+1);
 
-void printStack(stack<int> stack){
-    while(!stack.empty()){
-        int s = stack.top();
-        stack.pop();
-        cout << s << " ";
-    }
-    cout << endl;
-}
+            //add elements back to stack except mid element
+            if(curr != n/2)
+                stk.push(i);
+        }
+
+        void printStack(stack<int> stack){
+            while(!stack.empty()){
+                int s = stack.top();
+                stack.pop();
+                cout << s << " ";
+            }
+            cout << endl;
+        }
+};
 
 int main(){
+    RemoveMid rm;
     stack<int> stack;
 
-    for(int i = 0; i < 7; i++)
+    for(int i = 1; i <= 6; i++)
         stack.push(i);
 
-    printStack(stack);
+    rm.printStack(stack);
 
-    removeMid(stack, stack.size());
+    rm.removeMidRec(stack, stack.size());
+    //rm.removeMidIter(stack);
 
-    printStack(stack);
+    rm.printStack(stack);
     return 0;
 }
