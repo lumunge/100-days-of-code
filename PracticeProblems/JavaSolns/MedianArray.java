0a29e1cc96a2445cdfeff15d6adc4e3092323930
Erick Lumunge <lumungep12@gmail.com>
2021-12-16 09:12:25 +0300

    median of two sorted merged arrays

---
+++ PracticeProblems/JavaSolns/MedianArray.java
@@ -0,0 +1,43 @@
+/*
+ * PROBLEM STATEMENT:
+ * Given two sorted arrays nums2 and nums2 of size m and n, return median of the two.
+ * Time complexity should be O(log(m+n))
+ *
+ * Approach.
+ * Median is the value separating the higher from the lower values.
+ * Merge the two sorted arrays to attain a merged sorted array
+ * If array length of new array is even,
+ *      median is the sum of two middle elements divided by two
+ * else
+ *      median is middle element, newArray[length/2]
+ * return median.
+ */
+
+class MedianArray{
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double median = 0;
+        int n1 = nums1.length, n2 = nums2.length;
+        int nums3[] = new int[n1+n2];
+        int i = 0, j = 0, k = 0;
+        //merge the two arrays to one sorted array
+        while(i < n1 && j < n2){
+            if(nums1[i] < nums2[j])
+                nums3[k++] = nums1[i++];
+            else
+                nums3[k++] = nums2[j++];
+        }
+        while(i < n1)
+            nums3[k++] = nums1[i++];
+        
+        while(j < n2)
+            nums3[k++] = nums2[j++];
+        //calculate the median 
+        int n3 = nums3.length;
+        if(n3 % 2 == 0){
+            median = (float)(nums3[n3/2] + nums3[(n3/2)-1]) / 2;
+        }else{
+            median = (float)nums3[n3/2];
+        }
+        return median;
+    }
+};
