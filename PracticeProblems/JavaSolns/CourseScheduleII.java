98e9f737b36a1d85c177c597aea48fdbe2abab9d
Erick Lumunge <lumungep12@gmail.com>
2021-12-23 20:28:23 +0300

    Courses Schedule

--- PracticeProblems/JavaSolns/CourseScheduleII.java
+++ PracticeProblems/JavaSolns/CourseScheduleII.java
@@ -1,3 +1,35 @@
+/* 
+ * PROBLEM STATEMENT
+ * Given numCourses courses, take courses labelled from 0 - numCourses-1.
+ * You are also given prerequisites where prerequisites[i] = [ai, bi] indicating that bi is
+ * a prerequisites for ai.
+ * Return the ordering of courses to take to finish all courses otherwise return an empty
+ * array.
+ *
+ * Sample.
+ * Input: numCourses = 4
+ *        prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+ * Output: [0, 2, 1, 3] / [0, 1, 2, 3]
+ * Explanation: Do course 0 then 1 or 2 then 2 or 1 then finish with course 3
+ *
+ * Approach (Topological Sort)
+ * Given the courses [[1,0],[2,0],[3,1],[3,2]] sort topologically.
+ * We have the following tree.      
+ *      0        
+ *    /   \
+ *   1     2
+ *    \    /
+ *       3
+ * Perform a depth first search and check for cycles, that is, no termination and return []
+ *
+ * Steps:
+ * 1. Build an adjacency list.
+ * 2. Perform a dfs with cycle detection and push elements to stack
+ *    stack is [3, 2, 1, 0]
+ * 3. This is the correct ordering [0, 1, 2, 3] or [0, 2, 1, 3]
+ * 4. If a cycle exists, return [], eg [0, 1, 2, 1]
+ */
+
 import java.util.Collections;
 import java.util.List;
 import java.util.LinkedList;
