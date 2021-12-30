8fd14d970f87112cb5ccead76e7751127c8ef5bf
Erick Lumunge <lumungep12@gmail.com>
2021-12-30 10:44:21 +0300

    smallest integer divisible by k

--- PracticeProblems/PySolns/SmallestDivisibleK.py
+++ PracticeProblems/PySolns/SmallestDivisibleK.py
@@ -1,3 +1,28 @@
+""" 
+PROBLEM STATEMENT:
+Given a positive integer k, find the length of the smallest positive integr n such that
+n is divisible by k and n only contains the digit 1.
+Return length of n, otherwise return -1.
+
+Sample:
+Input: k = 3
+Output: 3
+Explanation: 111 / 3 
+
+Input: k = 7
+Output: 6
+Explanation: 111111 / 7
+
+Approach.
+Initialize n as 0.
+Intitialize i, represents how many times we add 1 to find a divisible number
+also this is the length of n.
+While int i is less than or equal to k:
+    add '1's to n and divide by k,
+    if result is divisible by k, return i
+    otherwise increment i
+"""
+
 def SmallestDivisibleK(k):
     i = 1
     n = 0
