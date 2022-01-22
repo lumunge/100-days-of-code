91b33cbed6e7ff2c48c691e4cf62c08fe20fcbe6
Lumunge <lumungep12@gmail.com>
2022-01-22 18:04:23 +0300

    stone game dp[memoization] approach

--- PracticeProblems/JavaSolns/JanChallenge/StoneGameIV.java
+++ PracticeProblems/JavaSolns/JanChallenge/StoneGameIV.java
@@ -1,12 +1,22 @@
+//Leetcode 1510
+
+import java.util.HashMap;
+
 class StoneGameIV{
-	static boolean helper(int n){
+	static boolean helper(int n, HashMap<Integer, Boolean> memo){
 		if(n <= 0) return false;
+		if(memo.containsKey(n)) return memo.get(n);
 		for(int i = 1; i*i <= n; i++)
-		    if(!helper(n-(i*i))) return true;
+		    if(!helper(n-(i*i), memo)){
+			    memo.put(n, true);
+			    return true;
+		    }
+		memo.put(n, false);
 		return false;
 	}
-    	static boolean winnerSquareGame(int n) {
-        	return helper(n);
+    	static boolean winnerSquareGame(int n){
+		HashMap<Integer, Boolean> memo = new HashMap<>();
+        	return helper(n, memo);
 	}
 
 	public static void main(String[] args){
