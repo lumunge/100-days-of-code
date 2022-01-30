a0519f906fa91988e98d8c98cee46e33cb76bb31
lumunge <lumunge@localhost.localdomain>
2022-01-30 14:23:16 -0500

    rotate array

---
+++ PracticeProblems/JavaSolns/JanChallenge/RotateArray.java
@@ -0,0 +1,14 @@
+//Leetcode 189
+
+class RotateArray{
+    public void rotate(int[] nums, int k) {
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            result[(i+k) % nums.length] = nums[i];
+        }
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = result[i];
+        }
+        return;
+    }
+}
