de51a435d44c55687a2bb4a7f244b8e01e2a1904
lumunge <lumungep12@gmail.com>
2022-02-09 06:45:36 +0300

    k-diff pairs in array #532 - hsahmap implementation(linear)

--- PracticeProblems/JavaSolns/FebChallenge/KDiffPairs.java
+++ PracticeProblems/JavaSolns/FebChallenge/KDiffPairs.java
@@ -3,6 +3,8 @@
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
 
 class KDiffPairs{
     // using hashset
@@ -21,12 +23,33 @@ class KDiffPairs{
         return hset.size();
     }
 
+    // using hashmap
+    static int findPairsII(int[] nums, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        
+        // add to map
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        // traverse map
+        for(int i : map.keySet()){
+            // if k > 0 - check for i + k existence
+            // if k == 0 - check count of i in map
+            if(k > 0 && map.containsKey(i + k) || k == 0 && map.get(i) > 1)
+                count++;
+        }
+        
+        return count;
+    }
+
     public static void main(String[] args){
         int[] nums1 = {3, 1, 4, 1, 5};
         int[] nums2 = {1, 2, 3, 4, 5};
-        System.out.println(findPairsI(nums1, 2));
-        System.out.println(findPairsI(nums1, 0));
-        System.out.println(findPairsI(nums2, 1));
-        System.out.println(findPairsI(nums2, 0));
+        System.out.println(findPairsII(nums1, 2));
+        System.out.println(findPairsII(nums1, 0));
+        System.out.println(findPairsII(nums2, 1));
+        System.out.println(findPairsII(nums2, 0));
     }
 }
