ffe56aab353bd4295828ab09530495a51bd041b9
lumunge <lumungep12@gmail.com>
2022-02-10 06:51:09 +0300

    sub-array sum equals k - optimal with hashmap - linear time

--- PracticeProblems/JavaSolns/FebChallenge/SubArraySumK.java
+++ PracticeProblems/JavaSolns/FebChallenge/SubArraySumK.java
@@ -1,7 +1,9 @@
 // Leetcode #560 - Subarray sum equals K
 
+import java.util.HashMap;
+
 class SubArraySumK{
-    // brute force
+    // brute force - quadratic
     static int subArraySumI(int[] nums, int k){
         int count = 0, n = nums.length;
         for(int i = 0; i < n; i++){
@@ -14,14 +16,29 @@ class SubArraySumK{
         return count;
     }
 
+    // optimal - linear
+    static int subArraySumII(int[] nums, int k){
+        int count = 0, n = nums.length, sum = 0;
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        hmap.put(0, 1);
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            int x = sum - k;
+            if(hmap.containsKey(x))
+                count += hmap.get(x);
+            hmap.put(sum, hmap.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args){
         int[] arr1 = {1, 1, 1};
         int[] arr2 = {1, 2, 3};
         int[] arr3 = {1, -1, 0};
         int[] arr4 = {3, 4, 7, 2, -3, 1, 4, 2};
-        System.out.println(subArraySumI(arr1, 2)); // 2
-        System.out.println(subArraySumI(arr2, 3)); // 2
-        System.out.println(subArraySumI(arr3, 0)); // 3
-        System.out.println(subArraySumI(arr4, 7)); // 4
+        System.out.println(subArraySumII(arr1, 2)); // 2
+        System.out.println(subArraySumII(arr2, 3)); // 2
+        System.out.println(subArraySumII(arr3, 0)); // 3
+        System.out.println(subArraySumII(arr4, 7)); // 4
     }
 }
