3a892a4fb0ac01aaa9c5dfa9573647154c1b62ef
lumunge <lumungep12@gmail.com>
2022-03-21 05:10:35 +0300

    constructor and method overloading

--- OOP/GetStarted/SimpleLocation.java
+++ OOP/GetStarted/SimpleLocation.java
@@ -2,6 +2,13 @@ public class SimpleLocation{
     // member variables
     public double longitude;
     public double latitude;
+   
+    // OVERLOADING THE CONSTRUCTOR - two constructors taking different arguments
+    // default constructor - no parameters
+    public SimpleLocation(){
+        this.longitude = -120.78;
+        this.latitude = 32.9;
+    }
 
     // constructor method - class definition
     // called when object is created
@@ -16,9 +23,15 @@ public class SimpleLocation{
         return lat1+lat2+long1+long2;
     }
     
-    // this referes to the calling object - loc1
+    // this refers to the calling object - loc1
     public double distance(SimpleLocation other){
         return getDist(this.latitude, this.longitude, other.latitude, other.longitude);
+    }
+
+    // Overloading methods
+    // parameters must be different
+    public double distance(double otherLat, double otherLong){
+        // body here
     }
 }
 
