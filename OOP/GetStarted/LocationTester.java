2b72f26f971b8cd60a2fddcf6b1977ee7f625e94
lumunge <lumungep12@gmail.com>
2022-03-21 04:56:52 +0300

    Classes and Objects

--- OOP/GetStarted/LocationTester.java
+++ OOP/GetStarted/LocationTester.java
@@ -1,8 +1,15 @@
-public class LocationTester{
-    public static void main(String[] args){
-        SimpleLocation kenya = new SimpleLocation(243.9, -99.5);
-        SimpleLocation ghana = new SimpleLocation(56.4, -33.2);
+// Creating objects
 
-        System.out.println(kenya.distance(ghana));
+public class LocationTester{
+
+    public static void main(String[] args){
+        // creating new objects
+        // calls constructor
+        SimpleLocation loc1 = new SimpleLocation(243.9, -99.5);
+        SimpleLocation loc2 = new SimpleLocation(56.4, -33.2);
+        
+        // invokes distance method
+        // loc1 is the calling object - compare two distances
+        System.out.println("Distance between is: " + loc1.distance(loc2));
     }
 }
--- OOP/GetStarted/SimpleLocation.java
+++ OOP/GetStarted/SimpleLocation.java
@@ -1,17 +1,22 @@
 public class SimpleLocation{
+    // member variables
     public double longitude;
     public double latitude;
 
-    /* constructor */
+    // constructor method - class definition
+    // called when object is created
+    // no return type
     public SimpleLocation(double longtude, double latitude){
         this.longitude = longitude;
         this.latitude = latitude;
     }
-
+    
+    // method
     public double getDist(double lat1, double long1, double lat2, double long2){
         return lat1+lat2+long1+long2;
     }
-
+    
+    // this referes to the calling object - loc1
     public double distance(SimpleLocation other){
         return getDist(this.latitude, this.longitude, other.latitude, other.longitude);
     }
