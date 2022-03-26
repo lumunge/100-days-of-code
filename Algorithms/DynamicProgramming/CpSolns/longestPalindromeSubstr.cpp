50ac1385191205eed5f1e8d53f3d9019415d901e
lumunge <lumungep12@gmail.com>
2022-03-26 22:37:54 +0300

    longest palindromic substring - #5

---
+++ Algorithms/DynamicProgramming/CpSolns/longestPalindromeSubstr.cpp
@@ -0,0 +1,64 @@
+// Leetcode #5 - Longest palindromic substring
+
+#include<iostream>
+#include<cstring>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class Solution{
+    public:
+        string longest(string str){
+            int n = str.length();
+            
+            // create and fill dp array with true
+            bool dp[n][n];
+            memset(dp, 0, sizeof(dp));
+             // fill dp array with true
+            for(int i = 0; i < n; i++)
+                dp[i][i] = true;
+            
+            // left and right pointers
+            int r = 0, l = 1; // single char string is palindrome
+            
+            // fill first two diagonals - check if two chars form palindrome
+            for(int i = 0; i < n-1; i++){
+                // if first two characters are equal
+                if(str[i] == str[i+1]){
+                    dp[i][i+1] = true;
+                    r = i;
+                    l = 2;
+                }
+            }
+            // start from third character
+            for(int i = 3; i <= n; i++){
+                for(int j = 0; j < n - i + 1; j++){
+                    int k = j + i - 1;
+                    // if mid string is palindrome and border characters are same - it is palindrome
+                    if(dp[j+1][k-1] && str[j] == str[k]){
+                        dp[j][k] = true;
+                        // if current substr > original
+                        if(i > l){
+                            // move pointers
+                            r = j;
+                            l = i;
+                        }
+                    }
+                }
+            }
+            return str.substr(r, l);
+        }
+};
+
+int main(){
+    Solution soln;
+    cout << soln.longest("babad") << endl;
+    cout << soln.longest("aacabdkacaa") << endl;
+    cout << soln.longest("cbbd") << endl;
+    cout << soln.longest("b") << endl;
+    cout << soln.longest("ab") << endl;
+    cout << soln.longest("bb") << endl;
+    cout << soln.longest("ccc") << endl;
+    return 0;
+}
