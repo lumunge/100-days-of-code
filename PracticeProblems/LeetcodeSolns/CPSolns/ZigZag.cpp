3b8237e9e2e2de4e965ed61ed0b6a2122ed59e19
lumunge <lumungep12@gmail.com>
2022-06-25 23:31:07 +0300

    zigzag

---
+++ PracticeProblems/LeetcodeSolns/CPSolns/ZigZag.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class ZigZag{
+    public:
+    
+    void printArr(vector<int> arr){
+        for(int i = 0; i < arr.size(); i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    
+    vector<int> getZigZag(vector<int> arr){
+        vector<int> finals;
+        for(int i = 0; i < arr.size()-1; i++){
+            finals.push_back(arr[i+1] - arr[i]);
+        }
+        return finals;
+    }
+
+    int checkZigZag(vector<int> arr){
+        int count = 0;
+        for(int i = 0; i <= arr.size(); i++){
+            if(arr[i] >= 0 && arr[i+1] < 0 || arr[i] < 0 && arr[i+1] >= 0){
+                count++;
+                cout << arr[i] << " ";
+            }
+        }
+        cout << endl;
+        return count;
+    }
+};
+
+
+int main(){
+    ZigZag zigzag;
+    vector<int> arr = {1, 7, 4, 9, 2, 5};
+    zigzag.printArr(arr);
+    vector<int> result = zigzag.getZigZag(arr);
+    zigzag.printArr(result);
+    cout << zigzag.checkZigZag(result) << endl;
+    return 0;
+}
+
+
+
